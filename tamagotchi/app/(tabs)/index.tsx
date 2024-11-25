da3e6e22bd1e6e36b697bdf482dd505c6b34c7c2
amath53 <131832235+amath53@users.noreply.github.com>
2024-11-25 14:46:40 -0600

    imported charts for real water intake data for achievements pane

--- tamagotchi/app/(tabs)/index.tsx
+++ tamagotchi/app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import {Button, View, StyleSheet} from "react-native";
 import { router } from 'expo-router';
 import {ThemedView} from "@/components/ThemedView";
 import {ThemedText} from "@/components/ThemedText";
-
+import Charts from './Charts';
 
 export default function HomeScreen() {
 
